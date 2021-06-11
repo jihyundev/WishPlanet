472f89734110bcddd1b9f7f08bccc276a3731f51
jihyundev <aawlgus12@gmail.com>
2021-06-11 16:29:50 +0900

    feat: 위시플래닛 소개 페이지 생성

--- TimeCapsule/TimeCapsule/Configuration/Extension/UILabel.swift
+++ TimeCapsule/TimeCapsule/Configuration/Extension/UILabel.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 extension UILabel {
+    
     func setLineSpace(spacing: CGFloat) {
         if let text = self.text {
             let attributeString = NSMutableAttributedString(string: text)
@@ -27,4 +28,15 @@ extension UILabel {
             self.attributedText = attributeString
         }
     }
+    
+    func makeBold(targetString: String) {
+        let fontSize = self.font.pointSize
+        let font = UIFont.SpoqaHanSansNeo(.bold, size: fontSize)
+        let fullText = self.text ?? ""
+        let range = (fullText as NSString).range(of: targetString, options: String.CompareOptions.caseInsensitive)
+        let attributeStr = NSMutableAttributedString.init(string: fullText)
+        attributeStr.addAttribute(.font, value: font, range: range)
+        self.attributedText = attributeStr
+    }
+    
 }
--- TimeCapsule/TimeCapsule/More/WishplanetViewController.swift
+++ TimeCapsule/TimeCapsule/More/WishplanetViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class WishplanetViewController: UIViewController {
 
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var subLabel: UILabel!
+    @IBOutlet weak var container1: UIView!
+    @IBOutlet weak var container2: UIView!
+    @IBOutlet weak var container3: UIView!
+    @IBOutlet weak var container4: UIView!
+    @IBOutlet weak var container5: UIView!
+    
+    @IBOutlet weak var opensourceView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -22,6 +32,27 @@ class WishplanetViewController: UIViewController {
     fileprivate func setupUI() {
         self.title = ""
         self.view.backgroundColor = .mainPurple
+        container1.backgroundColor = .mainPurple
+        container2.backgroundColor = .mainPurple
+        container3.backgroundColor = .mainPurple
+        container4.backgroundColor = .mainPurple
+        container5.backgroundColor = .mainPurple
+        opensourceView.backgroundColor = .mainPurple
+        
+        descLabel.text = "위시플래닛은 \n하고싶은 일, 이루고 싶은 소원들wish을 \n지금은 아닌, 미지의 먼 미래 (미지의 우주 - 행성 planet)에 할 수 있도록 미리 담아두는 타임캡슐 서비스 입니다."
+        descLabel.font = UIFont.SpoqaHanSansNeo(.regular, size: 14)
+        descLabel.makeBold(targetString: "위시플래닛")
+        descLabel.setLineHeight(lineHeightMultiple: 1.31)
+        
+        subLabel.text = "위시플래닛이 사용자들의 소원이 이뤄질 수 있도록 \n함께 응원하겠습니다."
+        subLabel.font = UIFont.SpoqaHanSansNeo(.regular, size: 14)
+        subLabel.setLineHeight(lineHeightMultiple: 1.31)
     }
+    
+    @IBAction func opensourceButtonTapped(_ sender: Any) {
+        let vc = OpenSourceViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
 
 }
